de95c8432268a757304256dfa0b3b9bacff1f261
Martin-Adam <copperdog08@gmail.com>
2014-07-09 15:44:34 -0600

    Create JDBCLibrary.java

---
+++ cit262/JDBCLibrary.java
@@ -0,0 +1,62 @@
+/*
+||  Program name:     JDBCLibrary.java
+||  Created by:       Michael McLaughlin | Copyright 2002
+||  Creation date:    04/09/02
+||  History:
+|| ----------------------------------------------------------------------
+||  Date       Author                   Purpose
+||  --------   ----------------------   ---------------------------------
+||  dd/mm/yy   {Name}                   {Brief statement of change.}
+|| ----------------------------------------------------------------------
+||  Execution method: Instantiated as a class within a class.
+||  Program purpose:  Designed as a user defined Exception.
+*/
+
+// Class imports.
+import java.awt.*;            // Required for AWT widgets.
+import java.awt.event.*;      // Required for AWT events.
+
+// ------------------------------ Begin Class --------------------------------/
+
+// Class definition.
+public class JDBCLibrary
+{
+  // -------------------------- Class Variables ------------------------------/
+
+  // ------------------------- Begin Constructor -----------------------------/
+
+  /*
+  || The constructors of the class are:
+  || =========================================================================
+  ||  Access     Constructor Type  Constructor
+  ||  ---------  ----------------  -------------------------------------------
+  ||  public     Default           JDBCLibrary()
+  */
+
+  // -------------------------------------------------------------------------/
+
+  // Default constructor extends RuntimeException.
+  public JDBCLibrary() {}
+
+  // --------------------------- Begin Methods -------------------------------/
+
+  /*
+  || The class must be instantiated from another class instance:
+  || =========================================================================
+  ||  Return Type  Method Name                    Access     Parameter List
+  ||  -----------  -----------------------------  ---------  -----------------
+  */
+
+  // ---------------------------- End Methods --------------------------------/
+
+  // ------------------------- Begin Inner Class -----------------------------/
+
+  // -------------------------- End Inner Class ------------------------------/
+
+  // ------------------------- Begin Static Main -----------------------------/
+
+  // -------------------------- End Static Main ------------------------------/
+
+} // End of JDBCLibrary class.
+
+// ------------------------------- End Class ---------------------------------/
